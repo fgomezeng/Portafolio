80320d31ac0679374e05aaf613d1bb53c36645b9
fgomezeng <fernando.gomez@utp.edu.co>
2025-08-16 15:20:58 -0500

    Internationalization i18n added

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languageList = {
+    es: { nativeName: "Español"},
+    en: { nativeName: "English"}
+}
+
+export const labels = {
+  es: {
+    'aside.home': 'Inicio',
+    'aside.projects': 'Proyectos',
+    'aside.contact': 'Contacto'
+    },
+  en: {
+    'aside.home': 'Home',
+    'aside.projects': 'Projects',
+    'aside.contact': 'Contact'
+  }
+}
